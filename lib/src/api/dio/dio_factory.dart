f7855b51d1833bae0f2be9bb9a38b5676d0211b1
Kehinde Akeredolu <9991881+kayzkenny@users.noreply.github.com>
2023-05-25 07:50:49 +0100

    pin cert in interceptor

--- lib/src/api/dio/dio_factory.dart
+++ lib/src/api/dio/dio_factory.dart
@@ -1,4 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
 import 'package:zxplore_app/env/app_env.dart';
 import 'package:zxplore_app/src/api/dio/interceptors/api_interceprtor.dart';
 
@@ -11,7 +14,35 @@ class AssetMgmtDioFactory {
   final ApiInterceptor _interceptor;
 
   /// Creates a Dio client.
-  Dio create() => Dio(_createBaseOptions())..interceptors.add(_interceptor);
+  Future<Dio> create() async {
+    final dio = Dio(_createBaseOptions())..interceptors.add(_interceptor);
+
+    ByteData pemContent =
+        await rootBundle.load('assets/server_certificate.pem');
+    ByteData keyContent =
+        await rootBundle.load('assets/server_certificate_key.pem');
+    ByteData crtContent =
+        await rootBundle.load('assets/server_certificate.crt');
+    // String pem = String.fromCharCodes(pemContent.buffer.asUint8List());
+    dio.httpClientAdapter = IOHttpClientAdapter(
+      onHttpClientCreate: (_) {
+        SecurityContext securityContext = SecurityContext();
+        securityContext.setTrustedCertificatesBytes(
+            crtContent.buffer.asUint8List(),
+            password: 'P@ssw0rd1234');
+        securityContext.useCertificateChainBytes(
+            pemContent.buffer.asUint8List(),
+            password: 'P@ssw0rd1234');
+        securityContext.usePrivateKeyBytes(keyContent.buffer.asUint8List(),
+            password: 'P@ssw0rd1234');
+        HttpClient client = HttpClient(context: securityContext);
+        client.badCertificateCallback =
+            (X509Certificate cert, String host, int port) => true;
+        return client;
+      },
+    );
+    return dio;
+  }
 
   BaseOptions _createBaseOptions() => BaseOptions(
         baseUrl: _baseUrl,
@@ -25,7 +56,7 @@ class AssetMgmtDioFactory {
 /// Creates a Dio client.
 ///
 /// To used by the repositories.
-Dio createDio() {
+Future<Dio> createDio() async {
   final apiInterceptor = ApiInterceptor();
   final dioFactory = AssetMgmtDioFactory(AppEnv.baseUrl, apiInterceptor);
 
