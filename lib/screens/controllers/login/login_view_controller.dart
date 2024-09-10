97e2836253b3a4754ba58b2fe7686271ad75f0e7
leadpresence <noblefelix4@gmail.com>
2024-09-10 16:34:57 +0100

    login controller

---
+++ lib/screens/controllers/login/login_view_controller.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/services.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:zxplore_app/apis/repository/providers/auth_repo_provider.dart';
+import 'package:zxplore_app/models/epma_models/rennew_token_response.dart';
+import 'package:zxplore_app/utils/app_exception.dart';
+import 'package:zxplore_app/utils/preferences.dart';
+import 'package:zxplore_app/utils/shared_preference_keys.dart';
+part 'login_view_controller.g.dart';
+
+@riverpod
+class LoginController extends _$LoginController {
+  @override
+  FutureOr<void> build() {
+    // nothing to do
+  }
+  var prefs = Preference();
+
+  Future<dynamic> loginUser({
+    required VoidCallback? onSuccess,
+    required String? loginMode,
+    required String? username,
+    required String? password,
+  }) async {
+    // final sharedPreferences = ref.read(sharedPreferencesProvider).requireValue;
+    try {
+      state = const AsyncValue.loading();
+      final repo = ref.read(authRespositoryImplProvider);
+
+      final loginRes = await repo.loginRepo(
+          loginMode: loginMode!, username: username!, password: password!);
+
+      if (loginRes.status) {
+        state = AsyncValue.data(loginRes);
+        //  perssist the token here
+        await prefs.load();
+        prefs.setString(SharedPreferencesKeys.accessTokenKey, loginRes.data);
+        onSuccess!.call();
+        return loginRes;
+      } else {
+        if (loginRes.message == 'token expired/invalid' ||
+            loginRes.code == 401) {
+          renewToken(
+              onSuccess: onSuccess,
+              loginMode: loginMode,
+              username: username,
+              password: password);
+        } else {
+          throw AppException('${loginRes.message}');
+        }
+      }
+    } catch (e, s) {
+      state = AsyncError(e, s);
+      throw AppException('${s.toString()}');
+
+      // return null;
+    }
+  }
+
+  Future<RenewTokenResponse> renewToken({
+    required VoidCallback? onSuccess,
+    required String? loginMode,
+    required String? username,
+    required String? password,
+  }) async {
+    // final sharedPreferences = ref.read(sharedPreferencesProvider).requireValue;
+    try {
+      state = const AsyncValue.loading();
+      final repo = ref.read(authRespositoryImplProvider);
+
+      final renewTokenRes = await repo.renewToken(oldToken: '');
+
+      if (renewTokenRes.status) {
+        state = AsyncValue.data(renewTokenRes);
+        loginUser(
+            onSuccess: onSuccess,
+            loginMode: loginMode,
+            username: username,
+            password: password);
+        return renewTokenRes;
+      } else {
+        throw AppException('${renewTokenRes.message}');
+      }
+    } catch (e, s) {
+      state = AsyncError(e, s);
+      throw AppException('${s.toString()}');
+
+      // return null;
+    }
+  }
+}
---
+++ lib/screens/controllers/login/login_view_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_view_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginControllerHash() => r'46978e7793b5dec740204c1e44ff5c36e1791eb5';
+
+/// See also [LoginController].
+@ProviderFor(LoginController)
+final loginControllerProvider =
+    AutoDisposeAsyncNotifierProvider<LoginController, void>.internal(
+  LoginController.new,
+  name: r'loginControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$loginControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoginController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
