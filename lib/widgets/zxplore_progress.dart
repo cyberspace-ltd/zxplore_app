07685ceab8ff24aee0ae8820c2cf6aeee4e654f6
leadpresence <noblefelix4@gmail.com>
2024-09-10 16:32:56 +0100

    custom progress loader

---
+++ lib/widgets/zxplore_progress.dart
@@ -0,0 +1,26 @@
+ 
+import 'package:flutter/material.dart'; 
+import 'package:zxplore_app/widgets/blur_modal_progress.dart';
+
+/// Custom Adapative circular progress indicator
+class ZxploreProgress extends StatelessWidget {
+  const ZxploreProgress(
+      {super.key,
+      required this.child,
+      required this.inAsyncCall,
+      this.title = 'Loading...'});
+  final Widget child;
+  final bool? inAsyncCall;
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlurryModalProgressHUD(
+      blurEffectIntensity: 7,
+      color: Colors.white,
+      inAsyncCall: inAsyncCall,
+      progressIndicator: CircularProgressIndicator(),
+    child: child,
+    );
+  }
+}
