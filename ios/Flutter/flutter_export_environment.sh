0ff1755c9d4d1da0ca331303068a8f06935cf75d
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2024-09-12 05:53:19 +0100

    dependencies changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/chibbueze/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/chibbueze/zxplore"
+export "FLUTTER_ROOT=/Users/mac/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/zxplore"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
